97714ba00a31050ccf7ca20a26f561a3561e5c03
gil.00000032610@unicap.br <gil.00000032610@unicap.br>
2024-05-26 14:48:30 -0300

    2-opt swap

--- app/src/main/java/algorithm/analysis/tsp/project/travelingsalesmanproblem/TSP.java
+++ app/src/main/java/algorithm/analysis/tsp/project/travelingsalesmanproblem/TSP.java
@@ -1,22 +1,50 @@
 package algorithm.analysis.tsp.project.travelingsalesmanproblem;
 
 public class TSP {
+    private Integer[][] weightedGraph;
 
-    public void swap(Integer[] elements, int i, int j) {
+    public TSP(Integer[][] weightedGraph) {
+        this.weightedGraph = weightedGraph;
+    }
 
+    public Integer[] swapEdges(Integer[] elements, int first, int second) {
+        Integer[] swapped = new Integer[elements.length];
+        outerElementFill(elements, swapped, first, second);
+        int innerPos = first + 1;
+        for (int i = second; i > first; i--) {
+            swapped[innerPos++] = elements[i];
+        }
+        return swapped;
+    }
+
+    private void outerElementFill(Integer[] original, Integer[] outerElements, int first, int second) {
+        for (int i = 0; i <= first; i++) {
+            outerElements[i] = original[i];
+        }
+        for (int i = second; i < outerElements.length; i++) {
+            outerElements[i] = original[i];
+        }
     }
 
     public Integer[][] generateNeighborhood(Integer[] solution) {
         int solutionSize = solution.length;
-        int sizeOfNeighborhood = (solutionSize * (solutionSize - 1) / 2) - solutionSize;
-        Integer[][] newSolution = new Integer[sizeOfNeighborhood][solutionSize];
-        // implement 2-edge exchange or swap operator 
+        
+        Integer[] newSolution = new Integer[sizeOfNeighborhood][solutionSize];
+        // implement 2-edge exchange or swap operator
         return newSolution;
     }
 
     public Boolean acceptor(Integer[] currentSolution, Integer[] neighbor) {
         Boolean isBetter = false;
         return isBetter;
+    }
+
+    private int calculateDistanceCost(Integer[] solution) {
+        int cost = weightedGraph[solution[0]][solution[solution.length - 1]];
+        for (int i = 0; i < solution.length - 1; i++) {
+            cost += weightedGraph[solution[i]][solution[i + 1]];
+        }
+        return cost;
     }
 
 }
